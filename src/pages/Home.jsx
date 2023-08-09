e7bd7ea5e2d88f0ab0fe4f641c3864cd69d60ef4
Sina Moradian <sinacsharp@gmail.com>
2023-08-09 19:05:21 +0330

    Add content to the about and home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
-  return <section className={styles.main}>Home Page</section>;
+  return (
+    <div className="home-container">
+      <h1>You got the travel plans, we got the travel vans.</h1>
+      <p>
+        Add adventure to your life by joining the #vanlife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <Link to="vans">Find your van</Link>
+    </div>
+  );
 };
 
 export default Home;
