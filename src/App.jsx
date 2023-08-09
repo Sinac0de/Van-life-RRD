99d7b640b84975f947921892139525b2b44f72a7
Sina Moradian <sinacsharp@gmail.com>
2023-08-09 20:29:33 +0330

    Add server to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Vans from "./pages/Vans";
 
+import "./server";
 import "./App.css";
 
 function App() {
@@ -12,12 +14,14 @@ function App() {
           #VANLIFE
         </Link>
         <nav>
+          <Link to={"/vans"}>Vans</Link>
           <Link to="/about">About</Link>
         </nav>
       </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/vans" element={<Vans />} />
       </Routes>
     </BrowserRouter>
   );
