bc1554e2b646cbc39960334c586702c5a50ff00e
Sina Moradian <sinacsharp@gmail.com>
2023-08-12 11:35:33 +0330

    Create Dashboard, Income, and Reviews pages for the Host section

---
+++ src/pages/Host/Dashboard.jsx
@@ -0,0 +1,5 @@
+const Dashboard = () => {
+  return <h2>Dashboard is here!</h2>;
+};
+
+export default Dashboard;
---
+++ src/pages/Host/Income.jsx
@@ -0,0 +1,5 @@
+const Income = () => {
+  return <h2>Income is here!</h2>;
+};
+
+export default Income;
