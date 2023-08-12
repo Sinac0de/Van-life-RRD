7e8624559ebadf3353035da17f697ce3052ae526
Sina Moradian <sinacsharp@gmail.com>
2023-08-12 10:50:54 +0330

    Add Layout Route to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Vans from "./pages/Vans";
@@ -6,24 +6,18 @@ import VanDetail from "./pages/VanDetail";
 
 import "./server";
 import "./App.css";
+import Layout from "./components/Layout";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/" className="site-logo">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to={"/vans"}>Vans</Link>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
