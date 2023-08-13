5749829d534df643f59779c45d368fe742470bd9
Sina Moradian <sinacsharp@gmail.com>
2023-08-13 18:41:37 +0330

    Add fetch api and get van's data

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const HostVanDetail = () => {
-  return <h2>HostVanDetial is here!</h2>;
+  const [currentVan, setCurrentVan] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((res) => res.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, []);
+
+  if (!currentVan) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+      <img
+        src={currentVan.imageUrl}
+        alt={`Photo of ${currentVan.name}`}
+        width={"50px"}
+      />
+      <p>{currentVan.name}</p>
+      <p>${currentVan.price}</p>
+    </div>
+  );
 };
 
 export default HostVanDetail;
