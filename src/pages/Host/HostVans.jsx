39929494d89969b805bb5e804b962af37c9da0e9
Sina Moradian <sinacsharp@gmail.com>
2023-08-15 19:40:06 +0330

    Refactor absolute path

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -10,15 +10,9 @@ const HostVans = () => {
       .then((data) => setVans(data.vans));
   }, []);
 
-  console.log(vans);
-
   const hostVans = vans.map((van) => {
     return (
-      <Link
-        to={`/host/vans/${van.id}`}
-        key={van.id}
-        className="host-van-link-wrapper"
-      >
+      <Link to={van.id} key={van.id} className="host-van-link-wrapper">
         <div className="host-van-single">
           <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
           <div className="host-van-info">
