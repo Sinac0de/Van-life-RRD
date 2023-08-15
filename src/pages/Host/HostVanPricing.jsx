758e201b6d50f452f13e442d6e92b047aca37a0a
Sina Moradian <sinacsharp@gmail.com>
2023-08-15 19:39:41 +0330

    Fix the brackets typo of import useOutletContext

--- src/pages/Host/HostVanPricing.jsx
+++ src/pages/Host/HostVanPricing.jsx
@@ -1,4 +1,4 @@
-import useOutletContext from "react-router-dom";
+import { useOutletContext } from "react-router-dom";
 const HostVanPricing = () => {
   const { currentVan } = useOutletContext();
   return (
