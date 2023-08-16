068df713edb309a11afe0ed9da6712f134024c57
Sina Moradian <sinacsharp@gmail.com>
2023-08-16 23:02:02 +0330

    Add type & search states to the van link

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -20,7 +20,10 @@ const Vans = () => {
   const vanElements = displayedVans.map((van) => {
     return (
       <div key={van.id} className="van-tile">
-        <Link to={van.id}>
+        <Link
+          to={van.id}
+          state={{ search: searchParams.toString(), type: van.type }}
+        >
           <img src={van.imageUrl} />
           <div className="van-info">
             <h3>{van.name}</h3>
