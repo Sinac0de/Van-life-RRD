08863d5b3d6fc88eda9cc605b61847cac28161bf
Sina Moradian <sinacsharp@gmail.com>
2023-08-19 09:38:13 +0330

    Add throw error if response is not ok

--- src/api.js
+++ src/api.js
@@ -1,5 +1,12 @@
 export async function getVans() {
   const res = await fetch("/api/vans");
+  if (!res.ok) {
+    throw {
+      message: "Failed to fetch vans",
+      statsText: res.statusText,
+      status: res.status,
+    };
+  }
   const data = await res.json();
   return data.vans;
 }
